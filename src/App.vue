99a5aeb124640465f2909c98607d2ec68c6c5696
dragoocho <deagoocho@gmail.com>
2021-06-12 13:02:25 +0900

    redirect 속성과 router-link 까지 진행

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
+    <tool-Bar></tool-Bar>
+    <!-- 여기서는 케밥케이스 -->
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
+import ToolBar from "./components/ToolBar.vue";
 
+export default {
+  components: {
+    ToolBar, // 파스칼케이스 (컴포넌트 테그)
+  },
 };
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -11,10 +11,14 @@ Vue.use(VueRouter);
 export const router = new VueRouter({
   routes: [
     {
-      // path: url 주소
+      path: "/",
+      redirect: "/news",
+    },
+    {
       path: "/news",
-      // component: url 주소로 갔을 때 표시될 컴포넌트
+      // path: url 주소
       component: NewsView,
+      // component: url 주소로 갔을 때 표시될 컴포넌트
     },
     {
       path: "/ask",
