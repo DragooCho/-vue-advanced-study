5a927988f329bd1a48e928ba475d26f46e04ddf2
dragoocho <deagoocho@gmail.com>
2021-06-12 12:02:09 +0900

    'router-view를 이용한 라우팅 컴포넌트 표시'까지 진행

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+// import로 Vue, VueRouter를 불러오고 Vue.use(VueRouter)로 사용한다.
+import NewsView from "../views/NewsView.vue";
+import AskView from "../views/AskView.vue";
+import JobsView from "../views/JobsView.vue";
+
+Vue.use(VueRouter);
+
+// 이 뷰 인스턴스가 라우터의 내용을 담게된다.
+export const router = new VueRouter({
+  routes: [
+    {
+      // path: url 주소
+      path: "/news",
+      // component: url 주소로 갔을 때 표시될 컴포넌트
+      component: NewsView,
+    },
+    {
+      path: "/ask",
+      component: AskView,
+    },
+    {
+      path: "/jobs",
+      component: JobsView,
+    },
+  ],
+});
